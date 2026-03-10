2468dd0f8bfbb944c3471a7ead74b9a362bebfd3
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-10 07:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function findDomLevel() {
+    let element = document.getElementById("level");
+    let count = 0;
+
+    while (element) {
+        count++;
+        element = element.parentElement;
+    }
+
+    alert("The level of the element is: " + count);
+}
+
+findDomLevel();
